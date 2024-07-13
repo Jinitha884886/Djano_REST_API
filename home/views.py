b63b08a64443e7f6e00e381845f384a1f51e9342
jinitha <jinithakg@gmail.com>
2024-07-13 16:32:38 +0530

    new folders creates

--- home/views.py
+++ home/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+# www.  /api/index
+
+@api_view(["GET"])
+def index(request):
+    people_details = {
+        "name": "Jinitha",
+        "age": 24,
+        "job": "Junior software developer"
+    }
+
+    return Response(people_details)
