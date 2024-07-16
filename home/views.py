2a661b0bd4795f0a785f8067bc2ea81b498fd2e3
jinitha <jinithakg@gmail.com>
2024-07-16 16:39:43 +0530

    Added POST and PUT methods

--- home/views.py
+++ home/views.py
@@ -3,12 +3,19 @@ from rest_framework.response import Response
 
 # www.  /api/index
 
-@api_view(["GET"])
+@api_view(["GET", "POST", "PUT"])
 def index(request):
-    people_details = {
-        "name": "Jinitha",
-        "age": 24,
-        "job": "Junior software developer"
-    }
+    if request.method == "GET":
+        people_details = {
+            "name": "Jinitha",
+            "age": 24,
+            "job": "Junior software developer"
+        }
 
-    return Response(people_details)
+        return Response(people_details)
+    
+    elif request.method == "POST":
+        return Response("POST")
+    
+    elif request.method == "PUT":
+        return Response("PUT")
